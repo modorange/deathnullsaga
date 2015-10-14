6549ba5908a1ebc6c9e46aad6ac537325b394d5a
Martijn Vegter <martijn@Martijns-MacBook-Pro.local>
2015-10-14 10:17:40 +0200

    yaml scenes

--- game_states/play.rb
+++ game_states/play.rb
@@ -2,87 +2,66 @@ class Play < GameState
   def initialize
     super
 
-    @music = Gosu::Song.new("media/music/kostverloren.ogg")
+    @scene = YAML.load_file("scenes/01_briljant.yml")
+    @music = Gosu::Song.new("media/music/#{@scene['music']}.ogg")
     @music.play
 
     @player = Character.create(x: 200, y: 200, image: 'tnuz_48x64.png')
+    
+    @characters = {'player' => @player}
 
-    @arnie = Character.create(x: 254, y: 100, image: 'arnie_48x64.png')
-    @knaap = Character.create(x: 324, y: 100, image: 'knaap_48x64.png')
+    @scene['characters'].each do |name, coordinates|
+      @characters[name] = Character.create(x: coordinates['x'], y: coordinates['y'], image: "#{name}_48x64.png")
+    end
 
     @font = Gosu::Font.new($window, "Courier", 18)
 
     @string
 
-    @script = [
-
-      
-      [@player, "He knapen, alles goed?"],
-      
-      [@knaap, "Het gaat slecht, maar verder gaat het goed"],
-      [@arnie, "Kun je niet ergens anders gaan zitten?\n Je bent zeiknat!"],
-
-
-      [@player, "Ja. Omdat het in HAARLEM ALTIJD REGENT!!!"],
-      [@arnie, "Het regent altijd alleen maar als jij er bent..."],
-      [@knaap, "In het Hearlemfe stellen we prijs op onze rust.\n Heel anders dan die Lugubere Feesttent\n waar jij vandaan komt!"],
-      [@knaap, "Maar weet je waar het helemaal gedaan is met de rust?\n In Schoten!"],
-
-
-      [@player, "Hoezo dat dan?"],
-      [@knaap, "Arnie heeft een basgitaar gekocht!"],
-      [@player, "Een basgitaar? waarom dat opeens?"],
-      [@knaap, "KIM DEAL"],
-      [@arnie, "..."],
-
-      [@player, "Speelde jij geen gitaar vroeger, knaap?"],
-      [@knaap, "Jazeker, in het rampjaar.."],
-      [@player, "Ja ja rampjaar 1992 Arnhem rotjeugd blabla.\n Ik heb thuis ook nog een gitaar liggen.\n We gaan een band beginnen!"],
-      [@arnie, "AWESOME!!!"],
-      [@knaap, "Hmmm puik plan. We moeten een naam hebben.."],
-
-      [@knaap, "..."],
-      [@knaap, "Loden plaat die langzaam neerdaalt op mijn ziel?"],
-      [@player, "..."],
-      [@knaap, "De Blauwe Eierkokers?"],
-      [@arnie, "..."],
-      [@knaap, "Het moet iets met leegte zijn,\n een zwart gat.. Dev/Null..."],
-      [[@arnie, @player], "Deåth/Nüll!!!!"]
-
-    ]
+    @zoom = 1.0
 
     @index = 0
+
+    @script = @scene['dialog']
 
     @player.input = { [:holding_a, :holding_left, :holding_pad_left] => :move_left, 
                       [:holding_d, :holding_right, :holding_pad_right] => :move_right, 
                       [:holding_w, :holding_up, :holding_pad_up] => :move_up, 
                       [:holding_s, :holding_down, :holding_pad_down] => :move_down
                     }
-    self.input = {r: :say_next}
+    self.input = {
+      r: :say_next,
+      holding_q: :zoom_in,
+      holding_w: :zoom_out
+    }
+  end
+
+  def zoom_in
+    @zoom += 0.05
+  end
+
+  def zoom_out
+    @zoom -= 0.05
   end
 
   def say_next
-    @arnie.say ''
-    @knaap.say ''
-    @player.say ''
+    @characters.each do |key, value|
+      value.say ''
+    end
 
     unless @script[@index].nil?
-
-      actors = @script[@index].first
-        if actors.is_a? Array
-          actors.each do |actor|
-            actor.say @script[@index][1]
-          end
-        else
-          actors.say @script[@index][1]
-        end
+      @script[@index].each do |key, value|
+        @characters[key].say value
+      end
       @index += 1
     end
     
   end
 
   def draw
-    super
-    Image["level.png"].draw(-400, -100, 0)
+    scale(@zoom, @zoom, @player.x, @player.y) do
+      super
+      Image["level.png"].draw(-400, -100, 0)
+    end
   end
 end
